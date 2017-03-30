5cc19261bc1890260b8a49c82f2325e86abfa706
studente <studente@MAT-38>
2017-03-30 11:22:27 +0200

    Verifica1

---
+++ ertta/src/rutili/rutili.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rutili;
+
+/**
+ *
+ * @author studente
+ */
+public class rutili {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FinestraAvvio f = new FinestraAvvio();
+        f.setVisible(true);
+        f.setLocation(400, 10);
+    }
+
+}
